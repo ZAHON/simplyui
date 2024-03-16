13e5883e2535c2e364305471fc3f0fa20c670116
ZAHON <konrad.zahon@gmail.com>
2024-03-16 22:34:52 +0100

    feat(@simplyui/react): update Checkbox component

--- packages/simplyui-react/src/components/checkbox/checkbox-icon/checkbox-icon.tsx
+++ packages/simplyui-react/src/components/checkbox/checkbox-icon/checkbox-icon.tsx
@@ -1,26 +1,13 @@
 import type { CheckboxIconProps } from './checkbox-icon.types';
 import { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
+import { IconCheck } from '@/icons/icon-check';
 import { checkboxIconStyles } from './checkbox-icon.styles';
 
 export const CheckboxIcon = forwardRef<SVGSVGElement, CheckboxIconProps>((props, ref) => {
   const { className, ...others } = props;
 
-  return (
-    <svg
-      ref={ref}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 16 16"
-      fill="currentColor"
-      aria-hidden="true"
-      focusable="false"
-      role="presentation"
-      className={twMerge(checkboxIconStyles(), className)}
-      {...others}
-    >
-      <path d="M12.207 4.793a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-2-2a1 1 0 011.414-1.414L6.5 9.086l4.293-4.293a1 1 0 011.414 0z" />
-    </svg>
-  );
+  return <IconCheck ref={ref} role="presentation" className={twMerge(checkboxIconStyles(), className)} {...others} />;
 });
 
 CheckboxIcon.displayName = 'CheckboxIcon';
