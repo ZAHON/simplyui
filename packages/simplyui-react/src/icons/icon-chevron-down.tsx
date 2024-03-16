d05e50f61f863f62760467b760a3b27c4bbc8146
ZAHON <konrad.zahon@gmail.com>
2024-03-16 22:26:45 +0100

    feat(@simplyui/react): add IconChevronDown icon component

---
+++ packages/simplyui-react/src/icons/icon-chevron-down.tsx
@@ -0,0 +1,25 @@
+import type { IconProps } from './icon.types';
+import { forwardRef } from 'react';
+
+export const IconChevronDown = forwardRef<SVGSVGElement, IconProps>((props, ref) => {
+  return (
+    <svg
+      ref={ref}
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      focusable="false"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z"></path>
+      <path d="M6 9l6 6 6-6"></path>
+    </svg>
+  );
+});
+
+IconChevronDown.displayName = 'IconChevronDown';
--- packages/simplyui-react/src/icons/index.ts
+++ packages/simplyui-react/src/icons/index.ts
@@ -1,5 +1,6 @@
 export type { IconProps } from './icon.types';
 
 export { IconCheck } from './icon-check';
+export { IconChevronDown } from './icon-chevron-down';
 export { IconChevronUp } from './icon-chevron-up';
 export { IconSelector } from './icon-selector';
