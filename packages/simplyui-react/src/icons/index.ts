5826598b147ba0600b868200f26dbb7d44d0b630
ZAHON <konrad.zahon@gmail.com>
2024-03-16 20:45:51 +0100

    feat(@simplyui/react): add IconChevronUp icon component

--- packages/simplyui-react/src/icons/index.ts
+++ packages/simplyui-react/src/icons/index.ts
@@ -1,4 +1,5 @@
 export type { IconProps } from './icon.types';
 
 export { IconCheck } from './icon-check';
+export { IconChevronUp } from './icon-chevron-up';
 export { IconSelector } from './icon-selector';
