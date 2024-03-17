29c153dd94e54ea309d8ae39082516a89ce2456f
ZAHON <konrad.zahon@gmail.com>
2024-03-17 01:23:06 +0100

    feat(@simplyui/react): update CloseButton component

--- packages/simplyui-react/src/components/close-button/close-button-icon/close-button-icon.styles.ts
+++ packages/simplyui-react/src/components/close-button/close-button-icon/close-button-icon.styles.ts
@@ -1,15 +1,12 @@
 import { cva } from 'class-variance-authority';
 
-export const closeButtonIconStyles = cva(
-  ['pointer-events-none', 'motion-safe:transition', 'motion-safe:duration-100'],
-  {
-    variants: {
-      size: {
-        '1': ['size-[0.9375rem]'],
-        '2': ['size-[1.125rem]'],
-        '3': ['size-[1.375rem]'],
-        '4': ['size-[1.625rem]'],
-      },
+export const closeButtonIconStyles = cva(['pointer-events-none'], {
+  variants: {
+    size: {
+      '1': ['size-[0.9375rem]'],
+      '2': ['size-[1.125rem]'],
+      '3': ['size-[1.375rem]'],
+      '4': ['size-[1.625rem]'],
     },
-  }
-);
+  },
+});
--- packages/simplyui-react/src/components/close-button/close-button-icon/close-button-icon.tsx
+++ packages/simplyui-react/src/components/close-button/close-button-icon/close-button-icon.tsx
@@ -1,30 +1,13 @@
 import type { CloseButtonIconProps } from './close-button-icon.types';
 import { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
+import { IconX } from '@/icons/icon-x';
 import { closeButtonIconStyles } from './close-button-icon.styles';
 
 export const CloseButtonIcon = forwardRef<SVGSVGElement, CloseButtonIconProps>((props, ref) => {
   const { size, className, ...others } = props;
 
-  return (
-    <svg
-      ref={ref}
-      xmlns="http://www.w3.org/2000/svg"
-      aria-hidden="true"
-      focusable="false"
-      fill="none"
-      stroke="currentColor"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      strokeWidth="1.5"
-      viewBox="0 0 24 24"
-      className={twMerge(closeButtonIconStyles({ size }), className)}
-      {...others}
-    >
-      <path stroke="none" d="M0 0h24v24H0z"></path>
-      <path d="M18 6L6 18M6 6l12 12"></path>
-    </svg>
-  );
+  return <IconX ref={ref} className={twMerge(closeButtonIconStyles({ size }), className)} {...others} />;
 });
 
 CloseButtonIcon.displayName = 'CloseButtonIcon';
