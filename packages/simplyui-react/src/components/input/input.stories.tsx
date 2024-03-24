36159fa6be929a8ee42090b1874f301b5dc47a94
ZAHON <konrad.zahon@gmail.com>
2024-03-24 23:42:48 +0100

    feat(@simplyui/react): update stories for Input component

--- packages/simplyui-react/src/components/input/input.stories.tsx
+++ packages/simplyui-react/src/components/input/input.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Label } from '@/components/label';
+import * as Label from '@/components/label';
 import { Input } from '.';
 
 const meta: Meta<typeof Input> = {
@@ -32,9 +32,9 @@ export const WithLabel: Story = {
   render: ({ size, disabled, ...others }) => {
     return (
       <div style={{ display: 'flex', flexDirection: 'column', rowGap: '0.25rem' }}>
-        <Label htmlFor="test" size={size} disabled={disabled}>
+        <Label.Root htmlFor="test" size={size} disabled={disabled}>
           Search the docs…
-        </Label>
+        </Label.Root>
         <Input id="test" size={size} disabled={disabled} {...others} />
       </div>
     );
